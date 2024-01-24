dbb50fb74df6fb427d27b6348189093a56752703
Cassidy <62119269+systemc12ashe@users.noreply.github.com>
2024-01-24 17:38:59 -0500

    Update site to be unavailable

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -6,18 +6,11 @@ function App() {
     <div className="App">
       {/* <h1>Hello World!</h1> */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>🚧</h1>
         <p>
-          Yay!
+          Under Construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
