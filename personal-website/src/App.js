95aba78337d81d6d997d609709ba04ed650153f3
Cassidy <62119269+systemc12ashe@users.noreply.github.com>
2024-01-24 16:59:18 -0500

    undo changes

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,11 +1,11 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      {/* <header className="App-header">
+      {/* <h1>Hello World!</h1> */}
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
