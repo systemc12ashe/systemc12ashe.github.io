7e286403d5211fb5d68ad9b0ce36f98d9f0192de
Cassidy <62119269+systemc12ashe@users.noreply.github.com>
2024-04-05 01:05:31 -0400

    Updates

--- personal-website/src/SkillSection.js
+++
@@ -1,9 +0,0 @@
-export function SkillSection({skillList}) {
-    return(
-        <ul className="skillSection">
-            {Object.keys(skillList).map((key, i) => (
-                <li key = {i} className="skill">{skillList[key]}</li>
-            ))}
-        </ul>
-    )
-}
